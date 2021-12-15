ecfabf87e851e9c82aa8373ef58155f1fde77d6b
Lucian Smith <lpsmith@uw.edu>
2021-12-15 13:22:01 -0800

    Add timing and slight speedup: only recheck neighbors of modified values.

--- day15_part2.py
+++ day15_part2.py
@@ -1,10 +1,12 @@
 import copy
 import numpy as np
+import time
 
+t0 = time.time()
 grid = []
-#for line in open("day15_example.txt"):
+# for line in open("day15_example.txt"):
 for line in open("day15_input.txt"):
-#for line in open("day15_simple_break.txt"):
+# for line in open("day15_simple_break.txt"):
     line = line.strip()
     vec = []
     for digit in line:
@@ -28,37 +30,55 @@ risk = []
 for n in range(len(grid)*5):
     risk.append(row.copy())
 
+def checkOne(risk, row, col, revisit):
+    if row==0 and col==0:
+        risk[row][col] = 0
+    else:
+        orig = risk[row][col]
+        neighbors = []
+        neighborindices = []
+        if row > 0:
+            neighbors.append(risk[row-1][col])
+            neighborindices.append((row-1, col))
+        if col > 0:
+            neighbors.append(risk[row][col-1])
+            neighborindices.append((row, col-1))
+        if row < len(risk)-1:
+            neighbors.append(risk[row+1][col])
+            neighborindices.append((row+1, col))
+        if col < len(risk[0])-1:
+            neighbors.append(risk[row][col+1])
+            neighborindices.append((row, col+1))
+
+        risk[row][col] = min(neighbors) + lookup(grid, row, col)
+        if risk[row][col] < orig:
+            revisit.update(neighborindices)
+            # if orig != np.inf:
+            #     print(neighbors, orig, risk[row][col])
+    
+
 def onePass(risk):
-    redo = False
+    revisit = set()
     for row in range(len(risk)):
         for col in range(len(risk[0])):
-            if row==0 and col==0:
-                risk[row][col] = 0
-            else:
-                orig = risk[row][col]
-                neighbors = []
-                if row > 0:
-                    neighbors.append(risk[row-1][col])
-                if col > 0:
-                    neighbors.append(risk[row][col-1])
-                if row < len(risk)-1:
-                    neighbors.append(risk[row+1][col])
-                if col < len(risk[0])-1:
-                    neighbors.append(risk[row][col+1])
+            checkOne(risk, row, col, revisit)
+    return revisit
 
-                risk[row][col] = min(neighbors) + lookup(grid, row, col)
-                if risk[row][col] < orig:
-                    redo = True
-                    # if orig != np.inf:
-                    #     print(neighbors, orig, risk[row][col])
-    return redo
-
-redo = onePass(risk)
+risk[0][0] = 0
+revisit = onePass(risk)
 pnum = 0
-while (redo):
-    redo = onePass(risk)
+while (len(revisit)>0):
+    newset = set()
+    for (row, col) in revisit:
+        checkOne(risk, row, col, newset)
+    revisit = newset
+    print(revisit)
     pnum += 1
     print(pnum)
 
 
+# print(risk)
 print(risk[-1])
+
+t1 = time.time()
+print("elapsed time:", t1-t0)
